067d3df0e390a4bdd08cab6e0a87c4509610ed38
anaclaura <144921601+anaclaura@users.noreply.github.com>
2023-10-18 10:56:44 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play('');
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+listaDeTeclas[0].onclick = tocaSomPom
+
+while(contador < 9){
+    listaDeTeclas[contador].onclick = function(){
+        tocaSom('#som_tecla_clap');
+    }
+    contador = contador +
+}
